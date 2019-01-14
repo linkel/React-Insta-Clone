843a482f54711e288786bc799443b40344103a60
linkel <ironcarbide@gmail.com>
2019-01-14 15:22:19 -0800

    Added post footer with heart icon

--- instagram/src/components/PostContainer/Post.jsx
+++ instagram/src/components/PostContainer/Post.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const Post = (props) => {
     return (
         <div className="post">
-            <img src={props.image}/>
+            <div className="post-header">
+                <img className="thumbnail" alt="thumbnail" src={props.thumbnail}/>
+                {props.username}
+            </div>
+            <img alt="big" src={props.image}/>
+            <div className="post-footer">
+                <div className="post-footer-top">
+                    <div>
+                        <img className="icon-heart" alt="heart" src={require('../../img/heart.svg')}/>
+                        <img className="icon-speech" alt="speech" src={require('../../img/speech.png')}/>
+                    </div>
+                    <img className="icon-bookmark" alt="bookmark" src={require('../../img/bookmark.svg')}/>
+                </div>
+                <div className="post-footer-bottom">
+                    {props.likes} likes
+                </div>
+            </div>
         </div>
     )
 }
